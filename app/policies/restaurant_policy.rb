b264ca1311bedd566e57e1f62dfd962c9c9d183c
Tiago Santos <tiago@convertodigital.com>
2020-03-03 10:58:18 +0000

    finishing setup

--- app/policies/restaurant_policy.rb
+++ app/policies/restaurant_policy.rb
@@ -3,5 +3,10 @@ class RestaurantPolicy < ApplicationPolicy
     def resolve
       scope.all
     end
+
+    def show?
+      true
+    end
+
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Restaurant.destroy_all
+
+# user = User.first
+
+# Restaurant.create!(name: 'mcdonalds', address: 'lisboa', user: user )
+# Restaurant.create!(name: 'patodonalds', address: 'porto', user_id: user.id )
+# Restaurant.create!(name: 'outra porra', address: 'algarve', user_id: user.id )
+
+# p "Created #{Restaurant.count} restaurants"
+
+
+puts 'Cleaning database...'
+Comment.destroy_all
+Restaurant.destroy_all
+User.destroy_all
+
+puts 'Creating user...'
+user = User.create! :email => 'api_user@gmail.com', :password => 'api_user', :password_confirmation => 'api_user'
+
+puts 'Creating restaurant...'
+restaurant = Restaurant.create! :name => 'rexmortus', :address => '100 Bourke Street, Melbourne', :user => user
+
+puts 'Creating comments...'
+Comment.create! :restaurant => restaurant, :user => user, :content => 'Example Comment 1'
+Comment.create! :restaurant => restaurant, :user => user, :content => 'Example Comment 2'
+Comment.create! :restaurant => restaurant, :user => user, :content => 'Example Comment 3'
+
+puts 'Finished!'
